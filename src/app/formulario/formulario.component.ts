0b8596ec5cac5fb1a1d939f0f5b334875e314fed
VictorioPerez <vic.perez2682@gmail.com>
2024-10-09 14:05:14 -0300

    push

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,21 +1,48 @@
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { RestService } from '../services/rest.service';
+import { NgIf } from '@angular/common';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf, ZXingScannerModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css',
 })
 export class FormularioComponent {
   constructor(private restForm: RestService) {}
+  qrImageUrl: string | null = null; // Variable para almacenar la URL de la imagen del QR
+  information: string = "";
+  scannerEnabled: boolean = true;
+
+  public scanSuccessHandler($event: any) {
+    this.scannerEnabled = false;
+    this.information = "Espera recuperando información... ";
+    this.information = $event;
+  }
+
+  public enableScanner() {
+    this.scannerEnabled = !this.scannerEnabled;
+    this.information = "No se  detectado información de ningún código. Acerque un código QR para escanear.";
+  }
+
+
+
+
+
+
+
+
+
+
+
+
 
   onSubmit(userForm: NgForm) {
     const formData = {
       newAuthRangeDto: {
-        neighbor_id: 0, // Este campo se genera en el backend
         init_date: userForm.value.initDate,
         end_date: userForm.value.endDate,
         allowedDaysDtos: [
@@ -57,6 +84,20 @@ export class FormularioComponent {
       (response) => {
         console.log('Respuesta del servidor:', response);
         alert('Formulario enviado exitosamente');
+
+        /* Para generar QR */
+        this.restForm.postQRDATA(response).subscribe((responseQR) => {
+          console.log('respuesta QR servidor', responseQR);
+          alert('QR generado exitosamente');
+
+          this.restForm.getQrImage(responseQR.id).subscribe((responseIMGQR) => {
+            let img = URL.createObjectURL(responseIMGQR);
+            this.qrImageUrl = img;
+            console.log(this.qrImageUrl);
+            alert('imagen creada!');
+          });
+        });
+
         userForm.reset();
       },
       (error) => {
@@ -71,5 +112,14 @@ export class FormularioComponent {
         }
       }
     );
+  }
+
+  descargarQR(): void {
+    if (this.qrImageUrl) {
+      let enlaceDescarga = document.createElement('a');
+      enlaceDescarga.href = this.qrImageUrl;
+      enlaceDescarga.download = 'codigo-qr.png';
+      enlaceDescarga.click();
+    }
   }
 }
--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -7,11 +7,20 @@ import { UserAllowed } from '../UserAllowed';
   providedIn: 'root',
 })
 export class RestService {
-  private apiUrl = 'http://localhost:8090/visitors'; // URL de tu API
+  private apiUrl = 'http://localhost:8080/visitors'; // URL de tu API
+  private generateIMGQR = 'http://localhost:8080/api/visitor-qr/image';
 
   constructor(private http: HttpClient) {}
 
   postFormulario(data: any): Observable<any> {
     return this.http.post(this.apiUrl, data);
   }
+  postQRDATA(data: any):Observable<any>{
+    return this.http.post("http://localhost:8080/api/visitor-qr/generate",data)
+  }
+
+  getQrImage(id: number): Observable<Blob> {
+    return this.http.get<Blob>(`${this.generateIMGQR}/${id}`, { responseType: 'blob' as 'json' });
+  }
+  
 }
